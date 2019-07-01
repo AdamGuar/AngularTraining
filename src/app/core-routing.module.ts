2e53e15e0d0b6386f6de3a0a36a4ea34629f8b9c
Adam Wozniak <adamwozniak@ge.com>
2019-07-01 14:28:02 +0200

    Add menu navbar

--- src/app/core-routing.module.ts
+++ src/app/core-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './core/pages/home-page/home-page.component';
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { AppComponent } from './core/components/app/app.component';
+import { MenuComponent } from './core/components/menu/menu.component';
+import { HomePageComponent } from './core/pages/home-page/home-page.component';
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
