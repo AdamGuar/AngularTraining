b9ccded9db1536d48181de7b7118083261344d53
Adam Wozniak <adamwozniak@ge.com>
2019-07-04 09:47:54 +0200

    Sort posts by date

--- src/app/posts/components/post-list/post-list.component.ts
+++ src/app/posts/components/post-list/post-list.component.ts
@@ -14,4 +14,15 @@ export class PostListComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnChanges(){
+    console.log('this.posts',this.posts);
+    if(!this.posts) return;
+    this.posts.sort((post1,post2)=>{
+      const date1 = new Date(post1.created_time);
+      const date2 = new Date(post2.created_time);
+      if(date1.getTime() > date2.getTime()) return -1;
+      else if(date1.getTime() < date2.getTime()) return 1;
+      else return 0;
+    });
+  }
 }
